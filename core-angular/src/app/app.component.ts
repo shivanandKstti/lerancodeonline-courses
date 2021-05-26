8c3e395fa989eb4f02e6d18e8d52c499a0cf0d43
yasharth <yash@simplify3x.com>
2021-05-26 10:54:55 +0530

    understanding angular core concepts

---
+++ core-angular/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-root',
+    // template: `
+    //     <h1>App Component Loading... </h1>
+    // `,
+    templateUrl: './app.component.html',
+    // styles: [
+    //     `
+    //         h1 {
+    //             background-color: red;
+    //         }
+    //     `
+    // ]
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+}
---
+++ core-angular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        CardComponent
+    ],
+    imports: [
+        BrowserModule,
+    ],
+    exports: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
